17369de354920edc1c04ff24a0c79527b077d954
Zenfoldium <ashishsingh1501@gmail.com>
2023-06-04 03:22:00 +0530

    commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_icon=":happy:",page_title="Lezzggooo",layout="wide")
+
+
+st.header("HIIII")
+st.title("Data is best hehehh")
+st.write("this is the uides")
