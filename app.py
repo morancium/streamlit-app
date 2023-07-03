69cb1215b0880c743ea897ba3e8c54a2a55bd6af
Zenfoldium <ashishsingh1501@gmail.com>
2023-07-03 22:09:15 +0530

    added time

--- app.py
+++ app.py
@@ -1,63 +1,58 @@
 # Bring in deps
 import os 
-# from apikey import apikey 
-
+import time
 import streamlit as st 
 from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain 
-from langchain.memory import ConversationBufferMemory
-from langchain.utilities import WikipediaAPIWrapper 
-import chromadb
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
 from config import OPENAI_API_KEY
+persist_directory="full_db"
 embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-persist_directory="db"
 vectordb = Chroma(persist_directory=persist_directory,embedding_function=embeddings)
 llm = OpenAI(temperature=1, openai_api_key=OPENAI_API_KEY)
 
 chain = load_qa_chain(llm, chain_type="stuff")
 
 def final_output(query,llm=llm):
-    doc=vectordb.similarity_search(query)
-    # print(doc,"\n")
+    doc_search_time_start=time.time()
+    k=4
+    doc=vectordb.similarity_search(query,k=k)
+    print("number of similar docs used: ",k)
+    doc_search_time_end=time.time()
+    print("Time for doc search: ",doc_search_time_end-doc_search_time_start)
     source_url=[]
+    info_from_llm_start=time.time()
     info=chain.run(input_documents=doc, question=query)
-    # print(chain.run(input_documents=doc, question=query))
-    # print('\n\nSources:')
+    info_from_llm_end=time.time()
+    print("Time taken to generate response from context from llm: ",info_from_llm_end-info_from_llm_start)
     for source in doc:
         source_url.append(source.metadata["source"])
-        # print(source.metadata["source"])
-    # source_url=list(set(source_url))
     return source_url,info
-    # for i in source_url:
-    #     print(i)
-# query = "what are charts"
-# final_output(query)
+
 # App framework
-
-
 # -------- Frontend----------
 st.title('AI Assistant')
 prompt = st.text_input('Plug in your prompt here') 
 
-# Memory 
-title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
-script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
-
 # Show stuff to the screen if there's a prompt
 if prompt:
     with get_openai_callback() as cb:
-        source_url,info=final_output(prompt)
+        start = time.time()
+        source_url,info=final_output(prompt)    
+        end = time.time()
         st.write(info)
+        print("total time: ",end-start)
         print(cb)
-
     with st.expander('Source'): 
+        source_url_appending_start=time.time()
         for i in range(len(source_url)):
             prefix=source_url[:i]
             name=source_url[i]
             if (prefix.count(name)<=0):
                 st.info(name)
+        source_url_appending_end=time.time()
+        print("Time taken for appending: ",source_url_appending_end-source_url_appending_start)
+
+print("---------------------------------------------------")
