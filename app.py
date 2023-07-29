aa3b0ea1d8eea4400cb124f09291d22b77c262ca
Zenfoldium <ashishsingh1501@gmail.com>
2023-07-30 01:02:05 +0530

    added automatic scrip to refresh the db

--- app.py
+++ app.py
@@ -3,54 +3,51 @@ import os
 import time
 import streamlit as st 
 import langchain
-from langchain.llms import OpenAI
+from langchain.llms import OpenAI as llm_openai
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
-
-from gptcache import Cache
-from gptcache.manager.factory import manager_factory
-from gptcache.processor.pre import get_prompt
-from langchain.cache import GPTCache
-import hashlib
 from config import OPENAI_API_KEY
 
-from gptcache.adapter.langchain_models import LangChainLLMs
-import warnings
-
+from gptcache import Cache
+from gptcache.manager import get_data_manager, CacheBase, VectorBase
+from gptcache.embedding import OpenAI as cache_openai
+from gptcache.adapter.api import init_similar_cache
+from langchain.cache import GPTCache
+import hashlib
+OPENAI_API_KEY=OPENAI_API_KEY
+# SQLite
+# from langchain.cache import SQLiteCache
+# langchain.llm_cache = SQLiteCache(database_path=".langchain.db")
+llm = llm_openai(openai_api_key=OPENAI_API_KEY,temperature=0)
 persist_directory="full_db"
 embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 vectordb = Chroma(persist_directory=persist_directory,embedding_function=embeddings)
-llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
+
+# GPTCache
+openai = cache_openai()
+d=openai.dimension
+cache_base = CacheBase('sqlite')
+vector_base = VectorBase('chromadb', dimension=d)
+data_manager = get_data_manager(cache_base, vector_base)
+
+def get_hashed_name(name):
+    return hashlib.sha256(name.encode()).hexdigest()
+
+def init_gptcache(cache_obj: Cache, llm: str):
+    hashed_llm = get_hashed_name(llm)
+    init_similar_cache(embedding=openai ,data_manager=data_manager,cache_obj=cache_obj, data_dir=f"similar_cache_{hashed_llm}")
+
+langchain.llm_cache = GPTCache(init_gptcache)
 chain = load_qa_chain(llm, chain_type="stuff")
-    
-# ''' GPTcache
-# llm = LangChainLLMs(llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY))
-
-# def get_hashed_name(name):
-#     return hashlib.sha256(name.encode()).hexdigest()
-
-# def init_gptcache(cache_obj: Cache, llm: str):
-#     hashed_llm = get_hashed_name(llm)
-#     cache_obj.init(
-#         pre_embedding_func=get_prompt,
-#         data_manager=manager_factory(manager="map", data_dir=f"map_cache_{hashed_llm}"),
-#     )
-
-# langchain.llm_cache = GPTCache(init_gptcache)
-# '''
-
-from langchain.cache import SQLiteCache
-langchain.llm_cache = SQLiteCache(database_path=".langchain.db")
-
 
 def final_output(query,llm=llm):
-    doc_search_time_start=time.time()
     k=3
+    doc_search_time_start=time.time()
     doc=vectordb.similarity_search(query,k=k)
-    print("number of similar docs used: ",k)
     doc_search_time_end=time.time()
+    print("number of similar docs used: ",k)
     print("Time for doc search: ",doc_search_time_end-doc_search_time_start)
     source_url=[]
     info_from_llm_start=time.time()
@@ -66,11 +63,11 @@ def final_output(query,llm=llm):
 st.title('AI Assistant')
 prompt = st.text_input('Plug in your prompt here') 
 
-# Show stuff to the screen if there's a prompt
+# # Show stuff to the screen if there's a prompt
 if prompt:
     with get_openai_callback() as cb:
         start = time.time()
-        source_url,info=final_output(prompt)    
+        source_url,info=final_output(prompt)
         end = time.time()
         st.write(info)
         print("total time: ",end-start)
@@ -84,15 +81,9 @@ if prompt:
                 st.info(name)
         source_url_appending_end=time.time()
         print("Time taken for appending: ",source_url_appending_end-source_url_appending_start)
+# time1=time.time()
+# print(llm("who is the ceo of facebook"))
+# time2=time.time()
+# print("time taken: ", time2-time1)
 
 print("---------------------------------------------------")
-# llm = LangChainLLMs(llm=OpenAI(temperature=0))
-# '''
-# chain = load_qa_chain(llm, chain_type="stuff")
-# time1=time.time()
-# query = "what is bubble chart"
-# docs=vectordb.similarity_search(query,k=2)
-# print(chain.run(input_documents=docs, question=query))
-# time2=time.time()
-# print(time2-time1) 
-# '''
--- playground.py
+++ playground.py
@@ -1,44 +1,53 @@
-# Bring in deps
-import os 
-# from apikey import apikey 
+import requests
+from bs4 import BeautifulSoup
+import json
+url = "https://docs.vitaracharts.com/guideMapFeatures/defaultMaps.html"
+landing="https://docs.vitaracharts.com/guideAllCharts/about.html"
+base="https://docs.vitaracharts.com"
+tab_links=[]
+response = requests.get(landing)
+soup = BeautifulSoup(response.content, "lxml")
 
-import streamlit as st 
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain 
-from langchain.memory import ConversationBufferMemory
-from langchain.utilities import WikipediaAPIWrapper 
-import chromadb
-from langchain.vectorstores import Chroma
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.chains.question_answering import load_qa_chain
-from config import OPENAI_API_KEY
-# print(OPENAI_API_KEY)
-
-
-embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-persist_directory="db"
-vectordb = Chroma(persist_directory=persist_directory,embedding_function=embeddings)
-llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
-
-chain = load_qa_chain(llm, chain_type="stuff")
-
-def final_output(query,llm=llm):
-    doc=vectordb.similarity_search(query)
-    source_url=[]
-    info=chain.run(input_documents=doc, question=query)
-    for source in doc:
-        source_url.append(source.metadata["source"])
-
-    return source_url,info
-
-query = "what is bubble charts"
-source_url,info=final_output(query)
-# print(info)
-# # source_url=[1,2,1,3,3,4,5,6,1,2,3]
-# for i in range(len(source_url)):
-#     prefix=source_url[:i]
-#     name=source_url[i]
-#     if (prefix.count(name)<=0):
-#         print(name)
-# print(vectordb.get())
+# tabs= soup.find_all("nav",class_="nav nav-tabs")
+# tab_links_outer=tabs[0].find_all("a",class_="nav-item nav-link",href=True)
+# for i in tab_links_outer:
+#     tab_links.append(base+i['href'])
+# print(tab_links)
+tab_links=["https://docs.vitaracharts.com/guideAllCharts/about.html","https://docs.vitaracharts.com/guideGridFeatures/appearance.html","https://docs.vitaracharts.com/guideIBCSCommonFeatures/about.html","https://docs.vitaracharts.com/guideMapFeatures/defaultMaps.html","https://docs.vitaracharts.com/customization/about.html"]
+Meta_texts=[]
+Meta_json=[]
+for url in tab_links:
+    response = requests.get(url)
+    response.raise_for_status()  # Check if the request was successful
+    urls=[]
+    title=[]
+    soup = BeautifulSoup(response.content, "lxml")
+    tags= soup.find_all("nav",class_="nav nav-pills leftMenu")
+    for i in range(len(tags)):
+        m=tags[i].find_all("a",href=True)
+        for j in range(len(m)):
+            title.append(m[j].text)
+            print((m[j].text))
+            urls.append(base+m[j]['href'])
+    print(len(urls))
+    print(len(title))
+    for i, url in enumerate(urls):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "lxml")
+        texts=soup.find_all("div",class_="col-md-9 rightContent")
+        body=texts[0].text
+        body=body.replace('\n','')
+        body=body.replace('\u2018','\'')
+        body=body.replace('\u2019','\'')
+        body=body.replace('\t','')
+        Meta_json.append({
+            "title":title[i],
+            "body":body,
+            "url":url
+        })
+        Meta_texts.append(body)
+        # break
+    json_data = json.dumps(Meta_json, indent=4)
+    with open('output4.json', 'w') as outfile:
+        json.dump(Meta_json, outfile)
+    # print(Meta_json)
